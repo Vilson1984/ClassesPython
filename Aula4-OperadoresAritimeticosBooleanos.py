95b48799473edfacec2e8ed068bf3886a42a95cb
Vilson1984 <vcmorion@hotmail.com>
2023-08-07 13:15:04 -0300

    Upping Aula4

---
+++ Aula4-OperadoresAritimeticosBooleanos.py
@@ -0,0 +1,26 @@
+# OPERADORES MATEMÁTICOS
+# Soma: + / Subtração: - / Multi: * / Divis: / 
+# Divisão inteira: //
+# Resto da divisão: %
+# Potência: **
+
+numero1 = 20
+numero2 = 10
+
+print(numero1-numero2)
+print(numero1+numero2)
+print(numero1*numero2)
+print(numero1/numero2)
+print(numero1//numero2)
+print(numero1%numero2)
+print(numero1**numero2)
+
+# OPERADORES BOOLEANOS
+# > 
+# <
+# <= 
+# >= 
+# == 
+# != 
+
+print ('Vai mostrar se é True ou False, se os números1 e 2 são diferentes >>>', numero1 != numero2)
