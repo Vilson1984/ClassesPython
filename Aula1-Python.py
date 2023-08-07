49628320ee85811ac5d259ee91183b5a880d93e1
Vilson1984 <vcmorion@hotmail.com>
2023-08-07 12:54:41 -0300

    Upping Aula1

---
+++ Aula1-Python.py
@@ -0,0 +1,18 @@
+# O HASHTAG É USADO PARA COMENTAR=> # 
+print('Hello world')
+print ('segundo print do vilson')
+
+# DECLARANDO VARIÁVEIS
+nome = "Vilson" #string
+idade = 39      #int
+altura = 1,72   #float
+estudante = True#boolean
+
+#Para imprimir o TIPO da variável
+print (type(estudante)) 
+
+#Para ARMAZENAR uma variável digitada pelo usuário => nomevar = input ("Texto de reuisição da var p/ usuário")
+linguagem = input('Digite o nome da linguagem estudada')
+
+#Para CONCATENAR e imprimir a var print('texto' , nomedavar) | print('a linguagem estudada é:' , linguagem )
+print ('A linguagem estudada é:', linguagem)
